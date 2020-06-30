c9a2ebc3393621c3af3b38a03165d24dc53b456a
kowalkowska-just <kowalkowska.just@gmail.com>
2020-06-30 17:37:50 +0200

    Complete app without auto layout

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageDice1: UIImageView!
+    @IBOutlet weak var imageDice2: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rollButton.backgroundColor = .clear
+        rollButton.layer.cornerRadius = 0.15 * rollButton.bounds.size.width
+        rollButton.layer.borderWidth = 1
+        rollButton.layer.borderColor = UIColor.white.cgColor
+        
+        
+        
+        
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+        imageDice1.image = diceArray.randomElement()
+        imageDice2.image = diceArray[Int.random(in: 0...5)]
+        
+        //Int.random(in: 0...5) is the same as randomElement().
+    }
 }
 
