40d268cb8ede85df1a66d2e0fc47455203b6e02a
kowalkowska-just <kowalkowska.just@gmail.com>
2020-06-30 16:27:09 +0200

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Justyna Kowalkowska on 30/06/2020.
+//  Copyright © 2020 Justyna Kowalkowska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
